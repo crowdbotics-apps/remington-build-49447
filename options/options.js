245158c2e9c9069c0a153102d7adb001456542a8
Crowdbotics <team@crowdbotics.com>
2024-09-20 21:20:41 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "remington-build-49447",
+  url: "https://remington-build-49447.botics.co",
+  api: "https://remington-build-49447.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
